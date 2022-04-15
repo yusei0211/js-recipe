4adbeaa65ba7db12edad30e03cc624e5291a5ed0
yusei0211 <saito31126@icloud.com>
2022-04-15 16:43:45 +0900

    [Trello (保存機能なし)]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,12 +1,20 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
-
+const mainasuButton = document.getElementById("mainasu-button")
 let count = 0
 
 // ボタン要素のonclickハンドラに関数を代入する
 plusButton.onclick = function () {
   // count を更新
   count += 1
+  // count を表示
+  display.textContent = count
+}
+
+// ボタン要素のonclickハンドラに関数を代入する
+mainasuButton.onclick = function () {
+  // count を更新
+  count -= 1
   // count を表示
   display.textContent = count
 }
